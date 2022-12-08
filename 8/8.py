958b8ae1c3043ebdf43ec02bcf6a1f5fba43a491
StijnMatsHendriks <stijnhendriks@protonmail.com>
2022-12-08 08:36:53 +0100

    formatting

--- 8/8.py
+++ 8/8.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def find_visible_tops(data, node):
     y, x = node
     height = data[x, y]
@@ -10,23 +11,27 @@ def find_visible_tops(data, node):
     scenic_score = find_num_trees_visible(left, right, up, down, height)
     top_visible = find_trees_not_blocked(top_visible, left, right, up, down, height)
     return top_visible, scenic_score
+
 def find_first(arr, height):
     for i in range(len(arr)):
         if arr[i] >= height:
             return i + 1
     return len(arr)
+
 def find_num_trees_visible(left, right, up, down, height):
     right_vis = find_first(right, height)
     left_vis = find_first(left[::-1], height)
     up_vis = find_first(up[::-1], height)
     down_vis = find_first(down, height)
     return right_vis * left_vis * up_vis * down_vis
+
 def find_trees_not_blocked(top_visible, left, right, up, down, height):
     for i in [left, right, up, down]:
         if np.all(i < height):
             top_visible += 1
             return top_visible
     return top_visible
+
 def walk_through_forest(data):
     visible_tops = get_boundary_size(data)
     scenic_score = []
@@ -37,6 +42,7 @@ def walk_through_forest(data):
             visible_tops += visible_tops_xy
             scenic_score.append((scenic_score_xy, (x, y)))
     return visible_tops, scenic_score
+
 def get_boundary_size(data):
     top_visible = data.shape[0] * 2 - 2 + data.shape[1] * 2 -2
     return top_visible
